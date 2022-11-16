33783c0dcba1e845f20cbb957ddc9fa00c5ef06e
Youcai Zhu <zhuycai1@163.com>
2022-11-16 11:17:19 +0800

    'update'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,69 @@
-let email = document.getElementById('email');
-let addBtn = document.getElementsByClassName('add');
-// let validFormat = /[a-zA-Z]*@[a-zA-Z].com/g;
+let form = document.forms[0];
+let email = form.email;
+let addBtn = form.addBtn;
+let errMsg = document.querySelector('.error-msg');
+let correctMsg = document.querySelector('.correct-msg');
+let addedMsg = document.querySelector('.added-msg');
+let validFormat = /^\w+@\w+\.com$/;
 
-addBtn.addEventListener('click', function() {
+// Verify that the email is formatted correctly.
+function isCorrect(str) {
+    return validFormat.test(str);
+}
 
+// Give a hint
+// error message
+function errHint() {
+    addedMsg.classList.remove('display');
+    correctMsg.classList.remove('display');
+    errMsg.classList.add('display');
+}
+// correct format message
+function correctHint() {
+    addedMsg.classList.remove('display');
+    errMsg.classList.remove('display');
+    correctMsg.classList.add('display');
+}
+// successfully added message
+function addedHint() {
+    errMsg.classList.remove('display');
+    correctMsg.classList.remove('display');
+    addedMsg.classList.add('display');
+}
+
+// clear all hint 
+function clear() {
+    errMsg.classList.remove('display');
+    correctMsg.classList.remove('display');
+    addedMsg.classList.remove('display');
+}
+
+// Test if the input field is empty
+function isEmpty(inputEle) {
+    return inputEle.value === '' ? true: false;
+}
+
+addBtn.addEventListener('click', function (evt) {
+    if (isCorrect(email.value)) {
+        addedHint();
+        return;
+    }
+    errHint();
+});
+
+email.addEventListener('input', () => {
+    if (isEmpty(email)) clear();
+});
+
+email.addEventListener('change', function () {
+    if (isEmpty(email)) return;
+    if (isCorrect(email.value)) {
+        correctHint();
+        return;
+    }
+    errHint();
+})
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 });
